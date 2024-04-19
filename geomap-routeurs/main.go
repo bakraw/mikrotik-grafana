f1669458160e15ccdf87e36b81d8a00d4b2156e6
bakraw <bkrav@protonmail.com>
2024-04-19 10:35:46 +0200

    Ajout fonction test statut adresse IP

--- geomap-routeurs/main.go
+++ geomap-routeurs/main.go
@@ -10,17 +10,19 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
+
+	probing "github.com/prometheus-community/pro-bing"
 )
 
 // Fait un appel à l'API renseignée.
 // Prend en entrée une adresse (string), renvoie le code de statut (int) et le corps ([]byte) de la réponse.
 // L'API Adresse du gouvernement est gratuite et fonctionne parfaitement pour la France (50 calls/IP/sec).
 // Si besoin d'utiliser une autre API, penser à changer la partie formatage et la fonction extractCoords.
-func geoAPI(addr string) (int, []byte) {
+func geoAPI(addr string) ([]byte, int) {
 
 	// Formatage de la requête
 	var addrConcat string = strings.ReplaceAll(addr, " ", "+")
-	fmt.Println(addrConcat)
 	var reqURL string = fmt.Sprintf("https://api-adresse.data.gouv.fr/search/?q=%s&limit=1", addrConcat) // Modifier en fonction de l'API à utiliser
 
 	// Exécution de la requête
@@ -35,7 +37,7 @@ func geoAPI(addr string) (int, []byte) {
 		log.Fatalf("--- Erreur lors du traitement de la réponse de l'API de géocodage:\n%s", err)
 	}
 
-	return res.StatusCode, resData
+	return resData, res.StatusCode
 }
 
 // Traite le JSON reçu et en extrait les coordonnées de l'adresse.
@@ -72,20 +74,65 @@ func extractCoords(data []byte) (float64, float64) {
 	return lat, lon
 }
 
+// Ping une adresse IP pour vérifier son état.ody
+// Prend en entrée un adresse IP (string) et renvoie le statut (int, up = 1 et down = 0).
+// On peut changer le nombre de paquets à envoyer et la durée avant time out.
+func probeIP(IPaddr string) int {
+	var up int
+
+	// Configuration du ping
+	pinger, err := probing.NewPinger(IPaddr)
+	if err != nil {
+		log.Fatalf("--- Erreur lors de la configuration du ping vers l'adresse spécifiée:\n%s", err)
+	}
+	pinger.Count = 1 // Nombre de paquets à envoyer.
+	pinger.SetPrivileged(true)
+	pinger.Timeout = time.Millisecond * 20 // Durée avant time out (en time.Duration).
+
+	// Exécution du ping
+	err = pinger.Run()
+	if err != nil {
+		log.Fatalf("--- Erreur lors de l'exécution du ping vers l'adresse spécifiée:\n%s", err)
+	}
+
+	// Résultat
+	if pinger.Statistics().PacketsRecv == pinger.Statistics().PacketsSent {
+		up = 1
+	} else {
+		up = 0
+	}
+
+	return up
+}
+
 // Pour l'instant, ne sert qu'à tester.
 // Servira probablement de menu.
 func main() {
-	var input string
+	var addrPost string
+	var addrIP string
 
+	// Récupération adresse postale
 	fmt.Print("Adresse >> ")
 	scanner := bufio.NewScanner(os.Stdin)
 	if scanner.Scan() {
-		input = scanner.Text()
+		addrPost = scanner.Text()
 	}
 
-	fmt.Println(input)
-	resCode, resBody := geoAPI(input)
-	fmt.Printf("%d\n%s\n", resCode, resBody)
+	// Récupération coordonnées géographiques
+	resBody, resCode := geoAPI(addrPost)
+	if resCode != 200 {
+		log.Fatalf("--- Erreur lors de l'appel à l'API de géocodage (code %d)", resCode)
+	}
 	lat, lon := extractCoords(resBody)
-	fmt.Print(lat, lon)
+	fmt.Printf("%f %f\n", lat, lon)
+
+	// Récupération adresse IP
+	fmt.Print("IP >> ")
+	if scanner.Scan() {
+		addrIP = scanner.Text()
+	}
+
+	// Traitement adresse IP
+	status := probeIP(addrIP)
+	fmt.Println(status)
 }
